1fa27903b84524611c91465a828844d92cbb2702
El tío judai <alexnarcista@gmail.com>
2026-01-08 12:41:22 -0600

    settings.js

---
+++ settings.js
@@ -0,0 +1,75 @@
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = "" //Ejemplo: 573218138672
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+"5214181450063",
+"50360438371",
+"5356795360"
+]
+
+global.suittag = ["5214181450063"] 
+global.prems = ["573114758599", "5214181450063"]
+global.fernando = ["5214181450063"]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = "Baileys Multi Device"
+global.vs = "^1.3"
+global.nameqr = "『Empire』"
+global.sessions = "Sessions/Principal"
+global.jadi = "Sessions/SubBot"
+global.AstaJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.botname = "Empire"
+global.textbot = "Empire • Powered By Judai"
+global.dev = "Powered By Judai"
+global.author = "Empire • Powered By Judai"
+global.etiqueta = "El Tio Judai"
+global.currency = "¥enes"
+global.banner = "https://files.atbox.moe/lajq7h.jpg"
+global.icono = "https://files.catox.moe/lajq7h.jpg"
+global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = "https:/"
+global.community = "http"
+global.channel = "https:"
+global.github = "https://github.c"
+global.gmail = "fer280l.com"
+global.ch = {
+ch1: "1203633er"
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.APIs = {
+xyro: { url: "https://xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
+vreden: { url: "https://api.vreden.web.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+adonix: { url: "https://api-adonix.ultraplus.click", key: 'Destroy-xyz' }
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("Update 'settings.js'"))
+import(`${file}?update=${Date.now()}`)
+})
